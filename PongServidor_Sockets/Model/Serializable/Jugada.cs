eaee2c08d21d4d593c70837ea1a4f4c771c5aafc
daniel <daniel.gil.saenz.trabajo@gmail.com>
2020-02-08 18:10:46 +0100

    MecagoenlosficherossinStageAll

---
+++ PongServidor_Sockets/Model/Serializable/Jugada.cs
@@ -0,0 +1,26 @@
+﻿using PongCliente_Sockets.Interfaces;
+using PongCliente_Sockets.MVC.Model.Serializable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PongServidor_Sockets.Model.Serializable
+{
+    class Jugada : Mostrar
+    {
+        public Jugada()
+        {
+        }
+
+        public Jugada(PlayerPos player1, Ball ball)
+        {
+            this.player1 = player1;
+            this.ball = ball;
+        }
+
+        public PlayerPos player1 { get; set; }
+        public Ball ball { get; set; }
+    }
+}
---
+++ PongServidor_Sockets/Model/Serializable/PlayerPos.cs
@@ -0,0 +1,20 @@
+﻿using PongCliente_Sockets.Interfaces;
+using PongCliente_Sockets.MVC.Model.Math_Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PongServidor_Sockets.Model.Serializable
+{
+    class PlayerPos : Mostrar
+    {
+        public PlayerPos(Point pos)
+        {
+            this.pos = pos;
+        }
+
+        public Point pos { get; set; }
+    }
+}
